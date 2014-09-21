c9b9f73f6a182aeba5081a39f948a52879823a73
Daniela Florescu <daniela.florescu91@yahoo.com>
2014-09-21 23:22:11 +0100

    wheel rotation

---
+++ finalProject/unityProject/Assets/Scripts/CarCameraScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCameraScript : MonoBehaviour {
+
+	public Transform car;
+	public float distance = 50.4f;
+	public float height = 15.4f;
+	public float rotationDamping = 3.0f;
+	public float heightDamping = 2.0f;
+	public float zoomRatio = 5.0f;
+	public float defaultFOV = 60;
+	private Vector3 rotationVector;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// LateUpdate is called once per frame after Update.
+	void LateUpdate () {
+		float wantedAngle = rotationVector.y;
+		float wantedHeight = car.position.y + height;
+		float myAngle = transform.eulerAngles.y;
+		float myHeight = transform.position.y;
+		myAngle = Mathf.LerpAngle (myAngle, wantedAngle, rotationDamping * Time.deltaTime);
+		myHeight = Mathf.Lerp (myHeight, wantedHeight, heightDamping * Time.deltaTime);
+		Quaternion currentRotation = Quaternion.Euler (0, myAngle, 0); 
+		transform.position = car.position;
+		transform.position -= currentRotation * Vector3.forward * distance; //put the camera at the back of the car.
+		transform.position = new Vector3 (transform.position.x, myHeight, transform.position.z-90);
+		transform.LookAt (car);
+	}
+
+	void FixedUpdate() {
+		Vector3 localVelocity = car.InverseTransformDirection (car.rigidbody.velocity);
+		if (localVelocity.z < -0.5f) {
+			rotationVector.y = car.eulerAngles.y + 180;
+		} else {
+			rotationVector.y = car.eulerAngles.y;
+		}
+
+		float acceleration = car.rigidbody.velocity.magnitude;
+		camera.fieldOfView = defaultFOV + acceleration * zoomRatio * Time.deltaTime;
+	}
+}
--- finalProject/unityProject/Assets/Scripts/CarControlScript.cs
+++ finalProject/unityProject/Assets/Scripts/CarControlScript.cs
@@ -7,6 +7,10 @@ public class CarControlScript : MonoBehaviour {
 	public WheelCollider WheelBL;
 	public WheelCollider WheelBR;
 	public WheelCollider WheelFR;
+	public Transform WheelFLTransform;
+	public Transform WheelFRTransform;
+	public Transform WheelBLTransform;
+	public Transform WheelBRTransform;
 	public float maxTorque = 50;
 
 	// Use this for initialization
@@ -22,5 +26,12 @@ public class CarControlScript : MonoBehaviour {
 		WheelBL.motorTorque = maxTorque * Input.GetAxis("Vertical");
 		WheelFL.steerAngle = 10 * Input.GetAxis("Horizontal");
 		WheelFR.steerAngle = 10 * Input.GetAxis("Horizontal");
+	}
+
+	void Update() {
+		WheelFLTransform.Rotate (WheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+		WheelFRTransform.Rotate (WheelFR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+		WheelBLTransform.Rotate (WheelBL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+		WheelBRTransform.Rotate (WheelBR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
 	}
 }
