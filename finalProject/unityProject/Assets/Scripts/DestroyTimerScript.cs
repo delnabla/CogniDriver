cd56407a4b21e27533af3c57226478822cc831ef
Daniela Florescu <daniela.florescu91@yahoo.com>
2014-09-28 00:07:19 +0100

    Skid marks

---
+++ finalProject/unityProject/Assets/Scripts/DestroyTimerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTimerScript : MonoBehaviour {
+
+	public float liveForSeconds = 2f;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer += Time.deltaTime;
+		if (timer > liveForSeconds)
+			Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ finalProject/unityProject/Assets/Scripts/SkiddingScript.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkiddingScript : MonoBehaviour {
+
+	public float currentFriction;
+	private float skidAt = 1.5f;
+	public GameObject skidSound;
+	public float soundEmition = 15;
+	private float soundWait;
+	public float markWidth = 1;
+	private int skidding;
+	private Vector3[] lastPos = new Vector3[2];
+	public Material skidMaterial;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		WheelHit hit;
+		WheelCollider wheel = (WheelCollider)transform.GetComponent ("WheelCollider");
+		wheel.GetGroundHit (out hit);
+		currentFriction = Mathf.Abs (hit.sidewaysSlip);
+		if (skidAt <= currentFriction && soundWait <= 0) 
+		{
+			Instantiate (skidSound, hit.point, Quaternion.identity);
+			soundWait = 1;
+		}
+		soundWait -= Time.deltaTime * soundEmition;
+		if (skidAt <= currentFriction) 
+		{
+			SkidMesh ();
+		} else {
+			skidding = 0;
+		}
+	}
+
+	void SkidMesh(){
+		WheelHit hit;
+		WheelCollider wheel = (WheelCollider)transform.GetComponent ("WheelCollider");
+		wheel.GetGroundHit (out hit);
+		GameObject mark = new GameObject ("Mark");
+		MeshFilter filter = mark.AddComponent<MeshFilter>();
+		mark.AddComponent<MeshRenderer>();
+		Mesh markMesh = new Mesh ();
+		Vector3[] vertices = new Vector3[4];
+		if (skidding == 0) {
+			skidding = 1;
+			vertices[0] = hit.point + Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z) * (new Vector3(markWidth, 0.01f, 0));
+			vertices[1] = hit.point + Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z) * (new Vector3(-markWidth, 0.01f, 0));
+		} else {
+			vertices[1] = lastPos[0];
+			vertices[0] = lastPos[1];
+		}
+		vertices[2] = hit.point + Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z) * (new Vector3(-markWidth, 0.01f, 0));
+		vertices[3] = hit.point + Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z) * (new Vector3(markWidth, 0.01f, 0));
+		lastPos [0] = vertices[2];
+		lastPos [1] = vertices[3];
+		int[] triangles = new int[6]{0, 1, 2, 2, 3, 0};
+		markMesh.vertices = vertices;
+		markMesh.triangles = triangles;
+		markMesh.RecalculateNormals();
+		Vector2[] uvmap = new Vector2[4];
+		uvmap [0] = new Vector2 (1, 0);
+		uvmap [1] = new Vector2 (0, 0);
+		uvmap [2] = new Vector2 (0, 1);
+		uvmap [3] = new Vector2 (1, 1);
+		markMesh.uv = uvmap;
+		filter.mesh = markMesh;
+		mark.renderer.material = skidMaterial;
+		mark.AddComponent<DestroyTimerScript>();
+	}
+
+}
