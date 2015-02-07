bf9eabe3a84f3134a4dc7f872ee2ef6524f9f8ea
Daniela Florescu <daniela.florescu91@yahoo.com>
2015-02-07 19:06:18 +0000

    Started making it rain

--- finalProject/unityProject/Assets/Scripts/EnvironmentScript.cs
+++ finalProject/unityProject/Assets/Scripts/EnvironmentScript.cs
@@ -3,14 +3,41 @@ using System.Collections;
 
 public class EnvironmentScript : MonoBehaviour {
 
+	public ParticleSystem rain;
+	public Material sunnySkybox;
+	public Material cloudySkybox;
+
 	// Use this for initialization
 	void Start () {
-	
+		rain.Stop();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Debug.Log ("Frustration: " + EmoAffectiv.frustrationScore);
+		if (EmoAffectiv.frustrationScore >= 0.5f && EmoAffectiv.shortTermExcitementScore <= 0.5f)
+			StartRain();
+		else 
+			StopRain();
+
 		Debug.Log ("Excitement: " + EmoAffectiv.longTermExcitementScore);
+	}
+
+	void StartRain()
+	{
+		if (!rain.isPlaying)
+		{
+			RenderSettings.skybox = cloudySkybox;
+			rain.Play ();
+		}
+	}
+
+	void StopRain()
+	{
+		if (rain.isPlaying)
+		{
+			rain.Stop();
+			RenderSettings.skybox = sunnySkybox;
+		}
 	}
 }
